54c3b84afddee9f55f9c313eeb627da9ecadc02c
Italo Miranda de Novais <italomirandadenovais@gmail.com>
2016-08-02 13:52:04 -0300

    Página de login incompleta

--- application/views/index.php
+++ application/views/index.php
@@ -1,15 +1,15 @@
 	<!-- Aqui e a area do conteudo -->
 	<div id="conteudo" class="container col-md-12">
-		<row>
+		<div class="row">
 			<div class="col-md-12">
 				<img src="<?php echo base_url('assets/img/logo-fake.png'); ?>">
 			</div>			
-		</row>
-		<row>
+		</div>
+		<div class="row">
 			<div class="col-md-12">
 				<button>Jogar</button>
 			</div>
-		</row>		
+		</div>		
 	</div> 
 </div>
 
--- application/views/login.php
+++ application/views/login.php
@@ -1,5 +1,9 @@
 <!-- Aqui e a area do conteudo -->
-			<div id="conteudo" class="container col-md-9">
-					<img src="<?php echo base_url('assets/img/logo-fake.png'); ?>">				
+			<div id="conteudo" class="container col-md-12">
+					<div class="row container">
+						<div id="painel-login" class="container col-md-12 warning">
+							<h1>Painel de login</h1>
+						</div>
+					</div>			
 			</div> 
 		</div>
