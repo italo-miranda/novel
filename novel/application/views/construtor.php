0c6fb897c4b642c5d75893e8d887bb91ff0a291a
Italo Miranda de Novais <italomirandadenovais@gmail.com>
2016-08-02 00:06:11 -0300

    Construtor das páginas html
    O arquivo construtor.php é responsável por chamar e construir as partes
    que compõem as páginas

---
+++ novel/application/views/construtor.php
@@ -0,0 +1,9 @@
+<?php
+$this->load->view('header');
+	if ($tela != 'index' && $tela != 'login' && $tela != 'recuperarsenha'):
+	 	$this->load->view('sidebar')
+     	$this->load->view($tela);
+	else :
+    	$this->load->view('index');
+	endif;
+$this->load->view('footer');
