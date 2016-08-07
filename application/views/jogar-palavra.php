907884dc8ec927219913f5730d23650034c75356
Italo Miranda de Novais <italomirandadenovais@gmail.com>
2016-08-07 11:51:51 -0300

    Carrossel pronto

--- application/views/jogar-palavra.php
+++ application/views/jogar-palavra.php
@@ -1,36 +1,207 @@
+
 <div id="conteudo" class="col-md-12 col-xs-12 well">
-	<div class="row" style="padding: 1%;">
-		<div class="col-md-8 col-xs-12 centered">
+	
 			<div class="row">
-				<div class="col-md-4 col-xs-12">
-					<h3>Complete com <?php //echo($tipoGrafema); ?></h3>
-				</div>
-				<div class="col-md-8 col-xs-12">
-					<span class="glyphicon glyphicon-stopwatch"></span>
-					<p><?php //echo($tempo); ?>Tempo</p>
-				</div>
+				<div id="myCarousel" class="carousel slide col-md-12 col-xs-12" data-ride="carousel" data-interval="false">
+					<div class="row" style="padding: 1%;">
+						<div class="col-md-8 col-xs-12 centered">
+			  				<!-- Em cada div abaixo irá uma questão-->
+							<div class="carousel-inner" role="listbox">
+								<!-- Questão 1-->
+								<div class="item active">
+									<div class="row">
+										<div class="col-md-4 col-xs-12">
+											<h3>Complete com <?php //echo($tipoGrafema); ?></h3>
+										</div>
+										<div class="col-md-8 col-xs-12">
+											<span class="glyphicon glyphicon-stopwatch"></span>
+											<p><?php //echo($tempo); ?>Tempo</p>
+										</div>
+									</div>
+
+									<div class="row centered">
+										<div class="col-md-4 col-xs-12">
+											<img src="<?php echo base_url('assets/img/palavra-fake.png'); ?>">
+										</div>
+										<div class="col-md-8 col-xs-12">
+											<p><?php //echo($enunciado); ?>Aqui vai o enunciado da questão 1</p>
+										</div>
+									</div>
+									
+									<div class="row centered">
+										<div class="col-md-12 col-xs-12">
+											<p>
+											<?php //echo($palavra1_metade1); ?>metade_1
+											<input type="text" class="input-sm"name="letra1">
+											<?php //echo($palavra1_metade2); ?>metade_2
+											</p>
+										</div>
+									</div>														
+								</div>
+
+								<!-- Questão 2-->
+								<div class="item">
+									<div class="row">
+										<div class="col-md-4 col-xs-12">
+											<h3>Complete com <?php //echo($tipoGrafema); ?></h3>
+										</div>
+										<div class="col-md-8 col-xs-12">
+											<span class="glyphicon glyphicon-stopwatch"></span>
+											<p><?php //echo($tempo); ?>Tempo</p>
+										</div>
+									</div>
+
+									<div class="row centered">
+										<div class="col-md-4 col-xs-12">
+											<img src="<?php echo base_url('assets/img/palavra-fake.png'); ?>">
+										</div>
+										<div class="col-md-8 col-xs-12">
+											<p><?php //echo($enunciado); ?>Aqui vai o enunciado da questão 2</p>
+										</div>
+									</div>
+									
+									<div class="row centered">
+										<div class="col-md-12 col-xs-12">
+											<p>
+											<?php //echo($palavra2_metade1); ?>metade_1
+											<input type="text" class="input-sm"name="letra1">
+											<?php //echo($palavra2_metade2); ?>metade_2
+											</p>
+										</div>
+									</div>														
+								</div>
+
+								<!-- Questão 3-->
+								<div class="item">
+									<div class="row">
+										<div class="col-md-4 col-xs-12">
+											<h3>Complete com <?php //echo($tipoGrafema); ?></h3>
+										</div>
+										<div class="col-md-8 col-xs-12">
+											<span class="glyphicon glyphicon-stopwatch"></span>
+											<p><?php //echo($tempo); ?>Tempo</p>
+										</div>
+									</div>
+
+									<div class="row centered">
+										<div class="col-md-4 col-xs-12">
+											<img src="<?php echo base_url('assets/img/palavra-fake.png'); ?>">
+										</div>
+										<div class="col-md-8 col-xs-12">
+											<p><?php //echo($enunciado); ?>Aqui vai o enunciado da questão 3</p>
+										</div>
+									</div>
+									
+									<div class="row centered">
+										<div class="col-md-12 col-xs-12">
+											<p>
+											<?php //echo($palavra3_metade1); ?>metade_1
+											<input type="text" class="input-sm"name="letra1">
+											<?php //echo($palavra3_metade2); ?>metade_2
+											</p>
+										</div>
+									</div>														
+								</div>
+
+								<!-- Questão 4-->
+								<div class="item">
+									<div class="row">
+										<div class="col-md-4 col-xs-12">
+											<h3>Complete com <?php //echo($tipoGrafema); ?></h3>
+										</div>
+										<div class="col-md-8 col-xs-12">
+											<span class="glyphicon glyphicon-stopwatch"></span>
+											<p><?php //echo($tempo); ?>Tempo</p>
+										</div>
+									</div>
+
+									<div class="row centered">
+										<div class="col-md-4 col-xs-12">
+											<img src="<?php echo base_url('assets/img/palavra-fake.png'); ?>">
+										</div>
+										<div class="col-md-8 col-xs-12">
+											<p><?php //echo($enunciado); ?>Aqui vai o enunciado da questão 4</p>
+										</div>
+									</div>
+									
+									<div class="row centered">
+										<div class="col-md-12 col-xs-12">
+											<p>
+											<?php //echo($palavra4_metade1); ?>metade_1
+											<input type="text" class="input-sm"name="letra1">
+											<?php //echo($palavra4_metade2); ?>metade_2
+											</p>
+										</div>
+									</div>														
+								</div>
+
+								<!-- Questão 5-->
+								<div class="item">
+									<div class="row">
+										<div class="col-md-4 col-xs-12">
+											<h3>Complete com <?php //echo($tipoGrafema); ?></h3>
+										</div>
+										<div class="col-md-8 col-xs-12">
+											<span class="glyphicon glyphicon-stopwatch"></span>
+											<p><?php //echo($tempo); ?>Tempo</p>
+										</div>
+									</div>
+
+									<div class="row centered">
+										<div class="col-md-4 col-xs-12">
+											<img src="<?php echo base_url('assets/img/palavra-fake.png'); ?>">
+										</div>
+										<div class="col-md-8 col-xs-12">
+											<p><?php //echo($enunciado); ?>Aqui vai o enunciado da questão 5</p>
+										</div>
+									</div>
+									
+									<div class="row centered">
+										<div class="col-md-12 col-xs-12">
+											<p>
+											<?php //echo($palavra5_metade1); ?>metade_1
+											<input type="text" class="input-sm"name="letra1">
+											<?php //echo($palavra5_metade2); ?>metade_2
+											</p>
+										</div>
+									</div>														
+								</div>	
+
+								<!--Botão responder-->
+								<div class="item">
+									<div class="row">
+										<div class="col-md-8 col-xs-12">
+											<h3>Clique em Responder para enviar as respostas!</h3>
+										</div>
+										<div class="col-md-4 col-xs-12">
+											<span class="glyphicon glyphicon-stopwatch"></span>
+											<p><?php //echo($tempo); ?>Tempo</p>
+										</div>
+									</div>
+									<div class="row centered">
+										<div class="col-md-12 col-xs-12">
+											<button type="submit" class="btn btn-success" name="letra">Responder</button>
+										</div>
+									</div>
+								</div>							
+							</div>	
+
+							<!-- Controles -->
+				  			<div class="row">
+								<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+									<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+									<span class="sr-only">Previous</span>
+								</a>
+								<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+									<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+									<span class="sr-only">Next</span>
+								</a>
+							</div>			  			
+						</div>
+					</div>
+				</div>	
 			</div>
-			<div class="row centered">
-				<div class="col-md-4 col-xs-12">
-					<img src="<?php echo base_url('assets/img/palavra-fake.png'); ?>">
-				</div>
-				<div class="col-md-8 col-xs-12">
-					<p><?php //echo($enunciado); ?>Aqui vai o enunciado da questão</p>
-				</div>
-			</div>
-			<div class="row centered">
-			<div class="col-md-12 col-xs-12">
-					<p>
-						<?php //echo($palavra_metade1); ?>metade_1
-						<input type="text" class="input-sm"name="letra">
-						<?php //echo($palavra_metade1); ?>metade_2
-					</p>
-				</div>
-			</div>	
-			<div class="row centered">
-				<div class="col-md-12 col-xs-12">
-					<button type="submit" class="btn btn-success" name="letra">Responder</button>
-				</div>
-			</div>		
-		</div>		
-	</div>	
+
+
+				
+
