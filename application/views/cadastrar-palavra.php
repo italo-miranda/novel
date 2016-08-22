7011d02b3207ef64212014dbd756166f9aa4c15d
Italo Miranda de Novais <italomirandadenovais@gmail.com>
2016-08-22 18:12:18 -0300

    correções no cadastri de palavra

--- application/views/cadastrar-palavra.php
+++ application/views/cadastrar-palavra.php
@@ -2,42 +2,70 @@
 			<div class="col-md-12 col-xs-12 afastado-1pc">
 				<div id="conteudo" class="col-md-8 col-xs-12 centered well" >
 					<div class="row">
-						<div id="cadastro-palavra" class="centered col-md-12 col-xs-12">
+						<div id="painel-login" class="col-md-12 col-xs-12">
 							<div class="row">
 								<h1>Cadastro de palavra</h1>
-							</div>
-							<form class="form-inline" role="form">
+							</div>							
+							<form class="form-horizontal" role="form">
+							  	<div class="row">
+							  		<div class="form-group">
+							  			<div class="col-md-4 col-xs-4">
+							  				<label for="enunciado">Enunciado:</label>
+							  			</div>
+								    	<div class="col-md-8 col-xs-8">
+								    		<textarea  style="width:100%" required="" maxlength= class="form-control" id="enunciado" name="enunciado"></textarea>
+								    	</div>								    
+							  		</div>
+							  	</div>
 							  	<div class="row">
 								  	<div class="form-group">
-								    	<label class="col-md-4 col-xs-4" for="enunciado">Enunciado:</label>
-								    	<input class="col-md-8 col-xs-8" type="textArea" class="form-control" id="enunciado" name="enunciado">
+								  		<div class="col-md-4 col-xs-4">
+									    	<label for="palavraIncompleta">Palavra incompleta:</label>
+									    </div>
+									    <div class="col-md-8 col-xs-8">
+								   			<input type="text" class="form-control" required="" size="40" id="palavraIncorreta" name="palavraIncompleta">
+								   		</div>
 								  	</div>
-								</div>
+							  	</div>
 							  	<div class="row">
 								  	<div class="form-group">
-								    	<label class="col-md-4 col-xs-4" for="palavraIncompleta">Palavra incompleta:</label>
-								   		<input class="col-md-8 col-xs-8" type="text" class="form-control" id="palavraIncompleta" name="palavraIncompleta">
+								  		<div class="col-md-4 col-xs-4">
+								    		<label for="gabarito">Gabarito:</label>
+								    	</div>
+								   		<div class="col-md-8 col-xs-8">
+								   			<input type="text" class="form-control" required=""  size="40" id="gabarito" name="gabarito">
+								   		</div>
 								  	</div>
-								</div>
-								<div class="row">
+							  	</div>
+							  	<div class="row">
 								  	<div class="form-group">
-								    	<label class="col-md-4 col-xs-4" for="gabarito">Gabarito:</label>
-								   		<input class="col-md-8 col-xs-8" type="text" class="form-control" id="gabarito" name="gabarito">
+								  		<div class="col-md-4 col-xs-4">
+								    		<label for="imagem">Imagem:</label>
+								    	</div>
+								   		<div class="col-md-8 col-xs-8">
+								   			<input type="file" class="form-control" required="" accept="image/*" id="imagem" name="imagem">
+								   		</div>
 								  	</div>
-								</div>
-								<div class="row">
+							  	</div>
+							  	<div class="row">
 								  	<div class="form-group">
-								    	<label class="col-md-4 col-xs-4" for="imagem">Imagem:</label>
-								   		<input class="col-md-8 col-xs-8" type="file" class="form-control" id="imagem" name="imagem" accept="image/*">
+								  		<div class="col-md-4 col-xs-4">
+								    		<label for="grafema">Selecione o grafema:</label>
+								    	</div>
+								   		<div class="col-md-8 col-xs-8">
+								   			<select name="grafema" id="grafema" class="form-control">
+												<option value="<?php echo('$grafema');?>"><?php echo('$grafema');?></option>
+												<option value="<?php echo('$grafema');?>"><?php echo('$grafema');?></option>
+												<option value="<?php echo('$grafema');?>"><?php echo('$grafema');?></option>
+												<option value="<?php echo('$grafema');?>"><?php echo('$grafema');?></option>
+											</select>
+								   		</div>
 								  	</div>
+							  	</div>
 								<div class="row">
-								<div class="row">
-									<button type="submit" class="btn btn-success">Começar</button>
+									<button type="submit" class="btn btn-success">Cadastrar</button>
 								</div>
-								<div id="botao-esqueciSenha">
-									<a href="<?php echo base_url('principal/recuperarSenha'); ?>" class="btn btn-link" role="button">Esqueci a senha</a>
-								</div>
-							</form>						
+							</form>																				
 						</div>
 					</div>
 				</div>
